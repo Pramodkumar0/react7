b65213ff205bf32bb7f4342445c38bd110114583
pramodkumar0 <pramodpusala@gmail.com>
2022-12-26 02:33:49 +0530

    ass7

---
+++ src/Data.jsx
@@ -0,0 +1,46 @@
+const Student=[
+    {
+        "id": 0,
+        "Name": "John",
+        "Age": "24",
+        "Course": "html",
+        "Batch": "October"
+    },
+    {
+        "id": 1,
+        "Name": "Doe",
+        "Age": "25",
+        "Course": "Css",
+        "Batch": "May"
+    },
+    {
+        "id": 2,
+        "Name": "Biden",
+        "Age": "26",
+        "Course": "Javascript",
+        "Batch": "September"
+    },
+    {
+        "id": 3,
+        "Name": "Barar",
+        "Age": "22",
+        "Course": "React",
+        "Batch": "December"
+    },
+    {
+        "id": 4,
+        "Name": "Christ",
+        "Age": 23,
+        "Course": "HTML",
+        "Batch": "April"
+    },
+    {
+        "id": 5,
+        "Name": "Elent",
+        "Age": 24,
+        "Course": "CSS",
+        "Batch": "November"
+    },
+]
+
+export default Student;
